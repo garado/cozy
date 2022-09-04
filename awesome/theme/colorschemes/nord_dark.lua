e9765d20e8d747a1e9d9c6f70ca5f1ccc641ea1e
Alexis Garado <alexisgarado@gmail.com>
2022-09-03 17:24:30 -0700

    restructure theme and colorscheme variables
    defining color variables and assigning colors to theme element variables have now been split into separate files. this makes it easier to handle changes to the list of theme elements.

--- awesome/theme/colorschemes/nord_dark.lua
+++ awesome/theme/colorschemes/nord_dark.lua
@@ -8,9 +8,11 @@ local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 local theme = dofile(themes_path .. "default/theme.lua")
 local theme_assets = require("beautiful.theme_assets")
-local xresources = require("beautiful.xresources")
-local dpi = xresources.apply_dpi
 local math = math
+
+-- █░█░█ ▄▀█ █░░ █░░ 
+-- ▀▄▀▄▀ █▀█ █▄▄ █▄▄ 
+theme.wallpaper = gears.surface.load_uncached(gfs.get_configuration_dir() .. "theme/assets/walls/nord_dark.png")
 
 -- █▀▀ █▀█ █░░ █▀█ █▀█ █▀
 -- █▄▄ █▄█ █▄▄ █▄█ █▀▄ ▄█
@@ -72,100 +74,14 @@ theme.subtext       = theme.nord2
 theme.main_accent   = theme.nord10  -- primary accent color
 
 -- misc (used in task, battery, and finance widgets)
-theme.red    = theme.nord11 -- 
-theme.green  = theme.nord14 --
+theme.red    = theme.nord11
+theme.green  = theme.nord14
 theme.yellow = theme.nord13
 
 -- changing colors up to this point should be all you need
 -- to change the entire color scheme.
 -- but if you want even more fine-grained color customization, 
 -- you can control the colors for almost every single UI element 
--- below.
-
-
--- █▀▀ █▀█ █▄░█ ▀█▀ █▀
--- █▀░ █▄█ █░▀█ ░█░ ▄█
-theme.font_name = "RobotoMono Nerd Font Mono "
-theme.font = theme.font_name .. "Regular "
-theme.alt_font_name = "Roboto "
-theme.alt_font = theme.alt_font_name .. "Regular "
-
-
-
--- █░█ █   █▀▀ █░░ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
--- █▄█ █   ██▄ █▄▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
--- Images
-theme.wallpaper = gears.surface.load_uncached(gfs.get_configuration_dir() .. "theme/assets/walls/nord_dark.png")
-
--- Dashboard
-theme.dash_bg         = theme.base
-theme.dash_widget_bg  = theme.crust
-theme.dash_tab_bg     = theme.crust
-theme.dash_header_fg  = theme.main_accent
-theme.dash_tab_fg     = theme.fg
-theme.fetch_title     = theme.main_accent
-theme.fetch_value     = theme.fg
-theme.habit_freq      = theme.main_accent
-theme.hab_uncheck_fg  = theme.fg
-theme.hab_uncheck_bg  = theme.nord0
-theme.hab_check_fg    = theme.fg
-theme.hab_check_bg    = theme.main_accent
-theme.timedate        = theme.main_accent
-theme.arcchart_colors = theme.accents
-theme.income_fg       = theme.green
-theme.expense_fg      = theme.red
-theme.legend_amount   = theme.subtitle
-theme.account_title   = theme.subtitle
-theme.album_filter_1  = theme.overlay
-theme.album_filter_2  = theme.overlay
-theme.now_playing_fg  = theme.fg
-theme.playerctl_bg    = theme.main_accent .. "00"
-theme.display_name_fg = theme.main_accent
-theme.title_fg        = theme.fg
-
--- Bar
-theme.wibar_bg        = theme.base
-theme.wibar_focused   = theme.main_accent
-theme.wibar_occupied  = theme.fg
-theme.wibar_empty     = theme.mantle
-theme.bat_charging    = theme.green
-theme.bat_normal      = theme.fg
-theme.bat_low         = theme.red
-theme.slider_bg       = theme.surface1
-theme.brightbar_fg    = theme.main_accent
-theme.volbar_fg       = theme.main_accent
-theme.wibar_clock     = theme.fg
-
--- Control center
-theme.ctrl_bg = theme.crust
-
--- Notifications
-theme.notif_bg          = theme.crust
-theme.notif_actions_bg  = theme.mantle
-theme.notif_timeout_bg  = theme.mantle
-theme.notif_dismiss_bg  = theme.transparent
-theme.notification_spacing = dpi(10)
-
--- Gaps
-theme.useless_gap = dpi(7)
-
--- Borders
-theme.border_width = dpi(3)
-theme.border_color_active = theme.main_accent
-theme.border_color_normal = theme.subtext
-
--- Corner radius
--- (not used for client rounding - used for rounding of other UI
--- components)
-theme.border_radius = 10
-
--- Hotkeys
-theme.hotkeys_bg = theme.crust
-theme.hotkeys_fg = theme.fg
-theme.hotkeys_modifiers_fg = theme.main_accent
-theme.hotkeys_border_width = dpi(0)
-theme.hotkeys_group_margin = dpi(25)
-theme.hotkeys_font = theme.font .. "13"
-theme.hotkeys_description_font = theme.alt_font .. "12"
+-- in theme.lua.
 
 return theme
--- awesome/theme/theme.lua
+++ awesome/theme/theme.lua
@@ -3,13 +3,102 @@
 
 local gears = require("gears")
 local gfs = require("gears.filesystem")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
 
 -- get user's color scheme
 local theme_name = require("user_variables").theme
 local theme = require("theme/colorschemes/" .. theme_name)
 
--- theme-agnostic settings
 theme.pfp = gears.surface.load_uncached(gfs.get_configuration_dir() .. "theme/assets/pfp.png")
 theme.transparent = "#ffffff00"
+
+-- █▀▀ █▀█ █▄░█ ▀█▀ █▀
+-- █▀░ █▄█ █░▀█ ░█░ ▄█
+theme.font_name = "RobotoMono Nerd Font Mono "
+theme.font = theme.font_name .. "Regular "
+theme.alt_font_name = "Roboto "
+theme.alt_font = theme.alt_font_name .. "Regular "
+
+-- █░█ █   █▀▀ █░░ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
+-- █▄█ █   ██▄ █▄▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
+-- Dashboard
+theme.dash_bg         = theme.base
+theme.dash_widget_bg  = theme.crust
+theme.dash_tab_bg     = theme.crust
+theme.dash_header_fg  = theme.main_accent
+theme.dash_tab_fg     = theme.fg
+theme.fetch_title     = theme.main_accent
+theme.fetch_value     = theme.fg
+theme.habit_freq      = theme.main_accent
+theme.hab_uncheck_fg  = theme.fg
+theme.hab_uncheck_bg  = theme.surface1
+theme.hab_check_fg    = theme.fg
+theme.hab_check_bg    = theme.main_accent
+theme.timedate        = theme.main_accent
+theme.arcchart_colors = theme.accents
+theme.income_fg       = theme.green
+theme.expense_fg      = theme.red
+theme.legend_amount   = theme.subtitle
+theme.account_title   = theme.subtitle
+theme.album_filter_1  = theme.overlay
+theme.album_filter_2  = theme.overlay
+theme.now_playing_fg  = theme.fg
+theme.playerctl_bg    = theme.main_accent .. "00"
+theme.display_name_fg = theme.main_accent
+theme.title_fg        = theme.fg
+
+-- Bar
+theme.wibar_bg        = theme.base
+theme.wibar_focused   = theme.main_accent
+theme.wibar_occupied  = theme.fg
+theme.wibar_empty     = theme.mantle
+theme.bat_charging    = theme.green
+theme.bat_normal      = theme.fg
+theme.bat_low         = theme.red
+theme.slider_bg       = theme.surface1
+theme.brightbar_fg    = theme.main_accent
+theme.volbar_fg       = theme.main_accent
+theme.wibar_clock     = theme.fg
+
+-- Control center
+theme.ctrl_bg = theme.crust
+theme.ctrl_host = theme.subtitle
+theme.ctrl_uptime = theme.subtitle
+theme.ctrl_link_fg = theme.fg
+theme.ctrl_link_bg = theme.surface0
+theme.ctrl_lowerbar_bg = theme.base
+theme.ctrl_power_options_bg = theme.mantle
+theme.ctrl_qa_btn_bg = theme.surface0
+theme.ctrl_power_options_btn_fg = theme.fg 
+
+-- Notifications
+theme.notif_bg          = theme.crust
+theme.notif_actions_bg  = theme.mantle
+theme.notif_timeout_bg  = theme.mantle
+theme.notif_dismiss_bg  = theme.notif_bg
+theme.notification_spacing = dpi(10)
+
+-- Gaps
+theme.useless_gap = dpi(7)
+
+-- Borders
+theme.border_width = dpi(3)
+theme.border_color_active = theme.main_accent
+theme.border_color_normal = theme.subtext
+
+-- Corner radius
+-- (not used for client rounding - used for rounding of other UI
+-- components)
+theme.border_radius = 10
+
+-- Hotkeys
+theme.hotkeys_bg = theme.crust
+theme.hotkeys_fg = theme.fg
+theme.hotkeys_modifiers_fg = theme.main_accent
+theme.hotkeys_border_width = dpi(0)
+theme.hotkeys_group_margin = dpi(25)
+theme.hotkeys_font = theme.font .. "13"
+theme.hotkeys_description_font = theme.alt_font .. "12"
 
 return theme
