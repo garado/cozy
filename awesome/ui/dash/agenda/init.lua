29f54ce770bfbfe59cb0a91f4ddab9a6bd094bc7
Alexis Garado <alexisgarado@gmail.com>
2022-10-21 18:05:28 -0700

    Dash:Agenda: View deadlines this week

--- awesome/ui/dash/agenda/init.lua
+++ awesome/ui/dash/agenda/init.lua
@@ -2,18 +2,30 @@
 -- █▀▄ ▄▀█ █▀ █░█ ▀    ▄▀█ █▀▀ █▀▀ █▄░█ █▀▄ ▄▀█ 
 -- █▄▀ █▀█ ▄█ █▀█ ▄    █▀█ █▄█ ██▄ █░▀█ █▄▀ █▀█ 
 
-local awful = require("awful")
-local gears = require("gears")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
+local colorize = require("helpers").ui.colorize_text
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
-local calendar = require("ui.dash.agenda.calendar")
-local upcoming = require("ui.dash.agenda.upcoming")
+local calendar  = require("ui.dash.agenda.calendar")
+local upcoming  = require("ui.dash.agenda.upcoming")
+local deadlines = require("ui.dash.agenda.deadlines")
+
+local header = wibox.widget({
+  markup = colorize("This week", beautiful.main_accent),
+  font = beautiful.alt_font_name .. "Light 30",
+  align = "center",
+  widget = wibox.widget.textbox,
+})
 
 local widget = wibox.widget({
-  calendar,
+  {
+    header,
+    calendar,
+    deadlines,
+    layout = wibox.layout.fixed.vertical,
+  },
   {
     upcoming,
     layout = wibox.layout.fixed.vertical,
