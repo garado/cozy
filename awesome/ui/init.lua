0712a44590a85e268ee87ff66c1741b022ed085d
Alexis Garado <alexisgarado@gmail.com>
2022-10-28 00:49:26 -0700

    Add layout list popup

--- awesome/ui/init.lua
+++ awesome/ui/init.lua
@@ -4,6 +4,7 @@ local bar = require(... .. ".bar")
 local dash = require(... .. ".dash")
 local control_center = require(... .. ".control_center")
 local switcher = require(... .. ".theme_switcher")
+local layoutlist = require(... .. ".layoutlist")
 
 -- Put bar on each screen
 local awful = require("awful")
@@ -12,4 +13,5 @@ awful.screen.connect_for_each_screen(function(s)
   dash(s)
   control_center()
   switcher()
+  layoutlist(s)
 end)
