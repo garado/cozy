619d478cffa0f911bbbb31944fb3fbfef237ea2c
Alexis Garado <alexisgarado@gmail.com>
2023-08-20 14:54:07 -0700

    fully move font specification to config file

--- cozyconf/init.lua
+++ cozyconf/init.lua
@@ -2,13 +2,122 @@
 -- █▀▀ █▀█ ▀█ █▄█ █▀▀ █▀█ █▄░█ █▀▀ 
 -- █▄▄ █▄█ █▄ ░█░ █▄▄ █▄█ █░▀█ █▀░ 
 
--- The various files in this folder are used to configure Cozy.
+-- Cozy configuration options.
 
-local gtable = require("gears.table")
+return {
 
-local config = {}
+  -- █░█ █
+  -- █▄█ █
 
-gtable.crush(config, require(... .. ".ui"))
-gtable.crush(config, require(... .. ".dash"))
+  -- Set desired theme and style.
+  theme_name  = "nord",
+  theme_style = "dark",
 
-return config
+  -- If Cozy's theme switcher should try to switch themes for other applications.
+  -- See `theme/integration.lua` for more details.
+  theme_switch_integration = true,
+
+  -- Font style to use.
+  -- Name/weight strings must have a trailing space!
+  font = {
+    name = "Circular Std ",
+    weights = {
+      light = "Light ",
+      reg   = "Regular ",
+      med   = "Medium ",
+      bold  = "Bold ",
+    },
+    sizes = {
+      xxs = 9,
+      xs  = 11,
+      s   = 13,
+      m   = 16,
+      l   = 22,
+      xl  = 28,
+      xxl = 35,
+    },
+  },
+
+  -- For scaling UI size up and down.
+  -- This only affects AwesomeWM widgets.
+  scale = 1,
+
+  -- Set bar style. Default style is vertical.
+  -- Options: horizontal vertical
+  bar_style = "horizontal",
+
+  -- Set bar alignment.
+  -- Options (horizontal): top bottom
+  -- Options (vertical): left right
+  bar_align = "top",
+
+
+  -- █▀▄▀█ █ █▀ █▀▀ 
+  -- █░▀░█ █ ▄█ █▄▄ 
+
+  -- GMT offset.
+  timezone = -7,
+
+
+  -- █▀▄ ▄▀█ █▀ █░█ 
+  -- █▄▀ █▀█ ▄█ █▀█ 
+
+  ---------------------------
+  -------- MAIN TAB ---------
+  ---------------------------
+
+  -- The name to display on the main tab.
+  name = "Alexis",
+
+  -- List of titles to display beneath profile picture.
+  titles = {
+    "Vim enthusiast",
+    "Mechromancer",
+  },
+
+  -- Icon shown at top of vbar and at bottom of dash sidebar.
+  distro_icon = "",
+
+  -- Selectable Timewarrior sessions.
+  timewarrior_sessions = {
+    "Applications",
+    "Cozy",
+    "Hobby",
+    "Personal",
+  },
+
+  -- Graph IDs of daily habits to track.
+  -- Used with Pixela: https://pixe.la/
+  habits = {
+    "coding",
+    "ledger",
+    "studying",
+    "journal",
+    "outside",
+    "music",
+  },
+
+  ----------------------------
+  --------- LEDGER -----------
+  ----------------------------
+
+  ledger = {
+    ledger_file  = "$HOME/Documents/Ledger/2023.ledger",
+    budget_file  = "$HOME/Documents/Ledger/budget.ledger",
+    account_file = "$HOME/Documents/Ledger/accounts.ledger",
+  },
+
+  ----------------------------
+  --------- CALENDAR ---------
+  ----------------------------
+
+  calendar = {
+    start_hour = 8,
+    end_hour   = 21, -- Shows through end hour
+  },
+
+  -- TODO: No other tabs implemented yet lol so this doesn't work
+  -- The default page to display when the calendar tab is open.
+  -- Options: week schedule overview
+  cal_default_tab = "overview",
+}
--- theme/init.lua
+++ theme/init.lua
@@ -9,9 +9,7 @@ local gtable   = require("gears.table")
 local clrutils = require("utils.color")
 
 -- Load default AwesomeWM theme
-local theme    = dofile(gfs.get_themes_dir() .. "default/theme.lua")
-
-theme.pfp      = gfs.get_configuration_dir() .. "theme/assets/pfp.png"
+local theme = dofile(gfs.get_themes_dir() .. "default/theme.lua")
 
 if conf.theme_switch_integration then
   require("theme.integration")
@@ -21,38 +19,31 @@ end
 -- █▀▀ █▀█ █▄░█ ▀█▀    █▀▀ █▀▀ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █ █▀█ █▄░█
 -- █▀░ █▄█ █░▀█ ░█░    █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █ █▄█ █░▀█
 
-local _font = require("theme.fonts." .. conf.fontset)
+local _font = conf.font
 local font = {}
 
-for w_name, w_val in pairs(_font.font_weights) do
-  for s_name, s_val in pairs(_font.font_sizes) do
-    local fval  = _font.font_name .. w_val .. s_val
+for w_name, w_val in pairs(_font.weights) do
+  for s_name, s_val in pairs(_font.sizes) do
+    local fval  = _font.name.. w_val .. dpi(s_val)
     local fname = 'font_' .. w_name .. '_' .. s_name
     font[fname] = fval
   end
 end
 
-for w_name, w_val in pairs(_font.alt_font_weights) do
-  for s_name, s_val in pairs(_font.alt_font_sizes) do
-    local fval  = _font.alt_font_name .. w_val .. s_val
-    local fname = 'altfont_' .. w_name .. '_' .. s_name
-    font[fname] = fval
-  end
-end
-
-
 -- █▀█ ▄▀█ █░░ █▀▀ ▀█▀ ▀█▀ █▀▀    █▀▀ █▀▀ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █ █▀█ █▄░█
 -- █▀▀ █▀█ █▄▄ ██▄ ░█░ ░█░ ██▄    █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █ █▄█ █░▀█
 
-local path           = "theme.colorschemes." .. conf.theme_name .. "." .. conf.theme_style
-local cscheme        = require(path)
+-- TODO: Needs major improvement.
+
+local path     = "theme.colorschemes." .. conf.theme_name .. "." .. conf.theme_style
+local cscheme  = require(path)
 
 -- Generate 7 primary colors
-local pbase          = cscheme.primary.base
+local pbase    = cscheme.primary.base
 
 -- For light-theme colorschemes, dark/light are inverted.
-local darken         = cscheme.type == "dark" and clrutils.darken or clrutils.lighten
-local lighten        = cscheme.type == "dark" and clrutils.lighten or clrutils.darken
+local darken   = cscheme.type == "dark" and clrutils.darken or clrutils.lighten
+local lighten  = cscheme.type == "dark" and clrutils.lighten or clrutils.darken
 
 cscheme.primary[900] = darken(pbase, 0.54)
 cscheme.primary[800] = darken(pbase, 0.48)
@@ -65,9 +56,9 @@ cscheme.primary[200] = lighten(pbase, 0.48)
 cscheme.primary[100] = lighten(pbase, 0.54)
 
 -- Generate 9 neutral colors
-local ndark          = cscheme.neutral.dark
-local nbase          = cscheme.neutral.base
-local nlight         = cscheme.neutral.light
+local ndark  = cscheme.neutral.dark
+local nbase  = cscheme.neutral.base
+local nlight = cscheme.neutral.light
 
 cscheme.neutral[900] = ndark
 cscheme.neutral[700] = clrutils.blend(ndark, nbase)
@@ -81,34 +72,34 @@ cscheme.neutral[400] = clrutils.blend(cscheme.neutral[300], nbase)
 cscheme.neutral[200] = clrutils.blend(nlight, cscheme.neutral[300])
 
 -- Generate 5 reds
-local red_base       = cscheme.colors.red
+local red_base = cscheme.colors.red
 
-cscheme.red          = {}
-cscheme.red[500]     = darken(red_base, 0.3)
-cscheme.red[400]     = darken(red_base, 0.15)
-cscheme.red[300]     = red_base
-cscheme.red[200]     = lighten(red_base, 0.15)
-cscheme.red[100]     = lighten(red_base, 0.3)
+cscheme.red      = {}
+cscheme.red[500] = darken(red_base, 0.3)
+cscheme.red[400] = darken(red_base, 0.15)
+cscheme.red[300] = red_base
+cscheme.red[200] = lighten(red_base, 0.15)
+cscheme.red[100] = lighten(red_base, 0.3)
 
 -- Generate 5 greens
-local green_base     = cscheme.colors.green
+local green_base = cscheme.colors.green
 
-cscheme.green        = {}
-cscheme.green[500]   = darken(green_base, 0.3)
-cscheme.green[400]   = darken(green_base, 0.15)
-cscheme.green[300]   = green_base
-cscheme.green[200]   = lighten(green_base, 0.15)
-cscheme.green[100]   = lighten(green_base, 0.3)
+cscheme.green = {}
+cscheme.green[500] = darken(green_base, 0.3)
+cscheme.green[400] = darken(green_base, 0.15)
+cscheme.green[300] = green_base
+cscheme.green[200] = lighten(green_base, 0.15)
+cscheme.green[100] = lighten(green_base, 0.3)
 
 -- Generate 5 yellows
-local yellow_base    = cscheme.colors.yellow
+local yellow_base = cscheme.colors.yellow
 
-cscheme.yellow       = {}
-cscheme.yellow[500]  = darken(yellow_base, 0.3)
-cscheme.yellow[400]  = darken(yellow_base, 0.15)
-cscheme.yellow[300]  = yellow_base
-cscheme.yellow[200]  = lighten(yellow_base, 0.15)
-cscheme.yellow[100]  = lighten(yellow_base, 0.3)
+cscheme.yellow = {}
+cscheme.yellow[500] = darken(yellow_base, 0.3)
+cscheme.yellow[400] = darken(yellow_base, 0.15)
+cscheme.yellow[300] = yellow_base
+cscheme.yellow[200] = lighten(yellow_base, 0.15)
+cscheme.yellow[100] = lighten(yellow_base, 0.3)
 
 function cscheme.random_accent_color()
   local i = math.random(1, #cscheme.accents)
@@ -117,31 +108,33 @@ end
 
 -----------
 
-theme.fg                   = cscheme.neutral[100]
+theme.pfp = gfs.get_configuration_dir() .. "theme/assets/pfp.png"
 
-theme.dash_widget_gap      = dpi(15)
+theme.fg = cscheme.neutral[100]
 
-theme.wibar_fg             = cscheme.neutral[100]
-theme.wibar_focused        = cscheme.primary[300]
-theme.wibar_empty          = cscheme.neutral[600]
-theme.wibar_occupied       = cscheme.neutral[100]
+theme.dash_widget_gap = dpi(15)
 
-theme.notif_bg             = cscheme.neutral[900]
-theme.notif_actions_bg     = cscheme.neutral[800]
-theme.notif_timeout_bg     = cscheme.neutral[800]
+theme.wibar_fg = cscheme.neutral[100]
+theme.wibar_focused = cscheme.primary[300]
+theme.wibar_empty = cscheme.neutral[600]
+theme.wibar_occupied = cscheme.neutral[100]
+
+theme.notif_bg = cscheme.neutral[900]
+theme.notif_actions_bg = cscheme.neutral[800]
+theme.notif_timeout_bg = cscheme.neutral[800]
 theme.notification_spacing = dpi(10)
 
-theme.wallpaper            = gears.surface.load_uncached(cscheme.wall)
+theme.wallpaper = gears.surface.load_uncached(cscheme.wall)
 
 -- Gaps
-theme.useless_gap          = dpi(7)
+theme.useless_gap = dpi(7)
 
 -- Borders
-theme.border_width         = dpi(3)
-theme.border_color_active  = cscheme.primary[300]
-theme.border_color_normal  = cscheme.neutral[900]
+theme.border_width = dpi(3)
+theme.border_color_active = cscheme.primary[300]
+theme.border_color_normal = cscheme.neutral[900]
 
-theme.border_radius        = 10
+theme.border_radius = dpi(10)
 
 -----------
 
